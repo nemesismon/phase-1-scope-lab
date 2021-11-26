56b74f7077e15a83e75e038c44bdd1d1a68aa26e
+Michael Kolb <+michael.w.kolb@gmail.com>
2021-11-26 15:47:56 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName (){
+    return customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+bestCustomer;
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavCust = 'prob bob';
+function changeLeastFavoriteCustomer () {
+    leastFavCust = 'definitely bob';
+}
